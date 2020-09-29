274a631aeb72d432d359ffe613c70041ea4a8284
fernando <fernando.a.l9607@gmail.com>
2020-09-29 01:08:29 -0500

    Updates

---
+++ precache-manifest.3542c7dd9380dc27151638037ce55adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5261790163a3e1aa2643bdd277128b4",
+    "url": "/rickandmortya-react/index.html"
+  },
+  {
+    "revision": "7bf9a90e4ca92bc075ef",
+    "url": "/rickandmortya-react/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "ea50e6927da6f0fbf56d",
+    "url": "/rickandmortya-react/static/css/main.32c72a3a.chunk.css"
+  },
+  {
+    "revision": "7bf9a90e4ca92bc075ef",
+    "url": "/rickandmortya-react/static/js/2.4348b8bb.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/rickandmortya-react/static/js/2.4348b8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea50e6927da6f0fbf56d",
+    "url": "/rickandmortya-react/static/js/main.1a495625.chunk.js"
+  },
+  {
+    "revision": "687e95e3a2546957a453",
+    "url": "/rickandmortya-react/static/js/runtime-main.0feb7e39.js"
+  },
+  {
+    "revision": "386b99717a71ee3f46cd3ace24f22b7f",
+    "url": "/rickandmortya-react/static/media/rickandmortya2.386b9971.png"
+  }
+]);
